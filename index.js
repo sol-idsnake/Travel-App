8ff62e2a802b24e08cd326ac825c6a66ad7ec26f
Hendrik Wendt <h.wendt@gmx.com>
2018-05-10 12:16:48 -0400

    feature google map autocomplete & feature wikipedia API added

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ function startApp(){
 }
 
 function inputListener(){
+	var input = document.getElementById('cityInput')
+	new google.maps.places.Autocomplete(input);
 	$('.js-form').submit(event => {
 		event.preventDefault()
 		const input = $('#cityInput').val()
 
 		fetchMap(input, displayMap)
+		fetchWiki(input, renderWiki)
 	})
 }
 
@@ -35,13 +38,11 @@ function fetchMap(city, callback){
 function displayMap(data){
 	const lat = data.results[0].geometry.location.lat
 	const long = data.results[0].geometry.location.lng
-	// console.log(`${lat} and ${long}`)
 
 	renderMaps(lat, long)
 }
 
 function renderMaps(lat, long){
-	console.log(lat, long)
 	$('.main').prop('hidden', false)
 	$('.googleMaps').prop('hidden', false)
 
@@ -55,4 +56,35 @@ function renderMaps(lat, long){
 	    map: map
 	});
 	
+}
+
+function fetchWiki(city, callback){
+	const query = {
+		url: 'https://en.wikipedia.org/w/api.php?action=query',
+		data: {
+			// action: 'query',
+			prop: 'extracts',
+			titles: city,
+			exsentences: 3,
+			formatversion: 2,
+			exintro: '',
+			explaintext: '',
+			redirects: '',
+			format: 'json',
+			// rvparse: 1,
+			origin: '*'
+		},
+		success: callback
+	}
+	$.ajax(query)
+}
+
+function renderWiki(data){
+	$('.wikipedia').prop('hidden', false)
+	$('.wikipedia').html(`
+		<h1>${data.query.pages["0"].title}</h1>
+		<p>${data.query.pages["0"].extract}</p>
+	`)
+	
+
 }
