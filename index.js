c2c88397dd852e2e63587db1be13f728b9e197c5
Hendrik Wendt <h.wendt@gmx.com>
2018-05-10 18:00:19 -0400

    feature weather API added

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function inputListener(){
 
 		fetchMap(input, displayMap)
 		fetchWiki(input, renderWiki)
+		fetchWeatherCity(input, renderWeather)
+
 	})
 }
 
@@ -35,12 +37,15 @@ function fetchMap(city, callback){
 	$.ajax(query)
 }
 
+let lat = ''
+let long = ''
 function displayMap(data){
-	const lat = data.results[0].geometry.location.lat
-	const long = data.results[0].geometry.location.lng
+	lat = data.results[0].geometry.location.lat
+	long = data.results[0].geometry.location.lng
 
 	renderMaps(lat, long)
 }
+
 
 function renderMaps(lat, long){
 	$('.main').prop('hidden', false)
@@ -54,8 +59,7 @@ function renderMaps(lat, long){
 	var marker = new google.maps.Marker({
 	    position: uluru,
 	    map: map
-	});
-	
+	});	
 }
 
 function fetchWiki(city, callback){
@@ -85,6 +89,32 @@ function renderWiki(data){
 		<h1>${data.query.pages["0"].title}</h1>
 		<p>${data.query.pages["0"].extract}</p>
 	`)
-	
+}
 
+function fetchWeatherCity(city, callback){
+	// setTimeout(function(){
+	const query = {
+		url: `https://api.openweathermap.org/data/2.5/weather`,
+		data: {
+			q: city,
+			appid: '5e5df2be1d3b2a169cf5895f07e31d61',
+			// origin: '*'
+		},
+		success: callback
+	}
+	$.ajax(query)
+	// }, 1000)
+}
+
+function renderWeather(data) {
+	$('.main').prop('hidden', false)
+	$('.weather').prop('hidden', false)
+
+	var temp = `${data.main.temp}`.substr(0, 2)
+
+	$('.weather').html(`
+		<h1>Weather for ${data.name}</h1>
+		<p>Currently ${data.weather["0"].description} in ${data.name}, at ${temp} &deg;C.</p>
+		<p>Wind speeds of ${data.wind.speed} mph, with ${data.main.humidity}% humidity.</p>
+	`)
 }
